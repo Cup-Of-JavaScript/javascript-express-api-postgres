c77cbbb07ea818993e571e7fd5500b1344735777
Martin Burolla <mburolla@xpanxion.com>
2022-07-11 16:56:32 -0400

    .gitignore is now working

--- postgres-pool.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg");
-
-exports.pool = new Pool({
-    user: "postgres",
-    password: "Ihgdp51505150!",
-    database: "stu0",
-    host: "localhost",
-    port: 5432,
-  });
