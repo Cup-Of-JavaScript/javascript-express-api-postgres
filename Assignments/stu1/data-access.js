d96f7d0e3dde3723dc9bb0f539a36e3a4b346970
Adraina Cheeks <programmingwithacheeks@xpanxion.com>
2022-07-13 17:11:35 -0400

    Getting Stock Person

--- Assignments/stu1/data-access.js
+++ Assignments/stu1/data-access.js
@@ -7,7 +7,7 @@
 const { pool } = require("../../postgres-pool");
 const GET_PERSON = "select * from person;"
 const GET_PERSONID = "select * from person where person_id = $1"
-const GET_STOCK_PERSON = "select p.person_id, pt.person_type_id, p.first_name, p.last_name, p.dob, pt.person_type from person p join person_type pt on p.person_type_id = pt.person_type_id where pt.person_type = $1"
+const GET_PERSON_TYPES = "select p.person_id, pt.person_type_id, p.first_name, p.last_name, p.dob, pt.person_type from person p join person_type pt on p.person_type_id = pt.person_type_id where pt.person_type = $1"
 
 // exports.ex1 = async () => {
 //     let personId = 1
@@ -39,7 +39,7 @@ exports.getPersonsID = async (personId) => {
 exports.GET_PERSONSFORPERSONTYPE = async (person_type) => {
     let retval = null;
     try {
-        let r = await pool.query(GET_STOCK_PERSON, [person_type]);
+        let r = await pool.query(GET_PERSON_TYPES, [person_type]);
         retval = r.rows;
     } catch (err) {
         console.error(err);
