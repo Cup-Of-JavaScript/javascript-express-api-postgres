903336f841c388434404b036783ebae7a76aa0eb
Michelle Gilyeat <mgilyeat@xpanxion.com>
2022-07-13 16:50:38 -0400

    Ex 4 Get Books

--- Assignments/stu8/data-access.js
+++ Assignments/stu8/data-access.js
@@ -11,6 +11,8 @@ const GET_PERSON = "select * from person where person_id =$1;"
 const GET_PERSON_TYPE = `select * from person p
 join person_type pt on pt.person_type_id = p.person_type_id where pt.person_type = $1`
 
+const GET_BOOKS = "select * from book"
+
 const { pool } = require("../../postgres-pool");
 
 exports.getPersons = async () => {
@@ -46,3 +48,13 @@ exports.getPersonType = async (personsType) => {
     return retval;
 }
 
+exports.getBooks = async () => {
+    let retval = null;
+    try {
+        let r = await pool.query(GET_BOOKS);
+        retval = r.rows;
+    } catch (err) {
+        console.error(err);
+    }
+    return retval;
+}
