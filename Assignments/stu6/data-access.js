6bc591c9244fbf6fb86a2e0290ad11d61c50f588
Joel Sipin <jsipin@xpanxion.com>
2022-07-14 15:15:17 -0400

    Ex2 Person ID

--- Assignments/stu6/data-access.js
+++ Assignments/stu6/data-access.js
@@ -1,11 +1,24 @@
 const { pool } = require("../../postgres-pool");
 
-const get_Persons = 'select * from person'
+//const get_Persons = 'select * from person'
+
+const PersonId = 'select * from person where person_id = ($1)'
 
 exports.getPersons = async () => {
     let retval = null;
     try {
-       let r = await pool.query(get_Persons);
+       let r = await pool.query(get_Person);
+       retval = r.rows
+    } catch (err) {
+        console.error(err);
+    }
+    return retval;
+}
+
+exports.getPerson = async (personId) => {
+    let retval = null;
+    try {
+       let r = await pool.query(PersonId, [personId]);
        retval = r.rows
     } catch (err) {
         console.error(err);
